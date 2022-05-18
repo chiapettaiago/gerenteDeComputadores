733c87c65dcee2454d2a02a546e498467e2cd8ff
Iago Chiapetta <iagochiapetta@gmail.com>
2022-05-18 09:11:21 -0300

    Adição de novos fluxos
    Fluxos de Sac adicionados e do Call Center

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from classes import *
 from PIL import Image
+import requests
 
 hide_menu_style = """
         <style>
@@ -27,6 +28,9 @@ cobranca1 = Computadores("Windows 10", "Intel Core 2 Quad", "3GB", "14 anos de i
 cobranca2 = Computadores("Windows 11", "Intel Pentium Gold", "8 GB", "3 anos de idade", "Sem Registro")
 cobranca3 = Computadores("Windows 11", "Intel Core i3", "6 GB", "6 anos de uso", "07 de abril de 2022")
 cobranca4 = Computadores("Windows 11", "Intel Core i5", "8 GB", "3 anos de idade", "07 de fevereiro de 2022")
+
+cx1 = Computadores("Windows 11", "Intel Pentium Gold", "8GB", "3 anos de idade", "07 de março de 2022")
+cx2 = Computadores("Windows 10", "Intel Pentium Gold", "8GB", "3 anos de idade", "27 de janeiro de 2022")
 
 if setor == "Cobrança":
     st.subheader('Setor de Cobrança')
@@ -64,15 +68,34 @@ if setor == "Cobrança":
 elif setor == "Selecionar...":
     st.markdown('Este software fornece gerenciamento computadorizado e automatizado a todos os computadores da Speed Fiber.')
 elif setor == "Call Center":
-    st.warning('Ainda estamos coletando esses dados. Aguarde só mais um pouco.')
+    image3 = Image.open('fluxos/Sala Sac Pronto.png')
+    st.image(image3, caption='Organização de computadores do Sac', use_column_width=True)
 elif setor == "Financeiro":
     st.warning('Ainda estamos coletando esses dados. Aguarde só mais um pouco.')
 elif setor == "Gerencia":
     st.warning('Ainda estamos coletando esses dados. Aguarde só mais um pouco.')
 elif setor == "Caixa":
-    st.warning('Ainda estamos coletando esses dados. Aguarde só mais um pouco.')
+    st.subheader('Caixas')
+    cxCol1, cxCol2 = st.columns(2)
+    with cxCol1:
+        st.subheader('CX 01')
+        st.text(cx1.os)
+        st.text(cx1.processador)
+        st.text(cx1.memoria)
+        st.text(cx1.idade)
+        st.text(cx1.dataFormatacao)
+    with cxCol2:
+        st.subheader('CX 02')
+        st.text(cx2.os)
+        st.text(cx2.processador)
+        st.text(cx2.memoria)
+        st.text(cx2.idade)
+        st.text(cx2.dataFormatacao)
+    imageCX = Image.open('fluxos/caixas.png')
+    st.image(imageCX, caption='Organização de computadores do Caixa', use_column_width=True)
 elif setor == "Atendimento SAC":
-    st.warning('Ainda estamos coletando esses dados. Aguarde só mais um pouco.')
+   imageAtendimentoSac = Image.open('fluxos/Atendimento Sac.png')
+   st.image(imageAtendimentoSac, caption='Organização de computadores do Caixa', use_column_width=True)
 elif setor == "Vendas":
     st.warning('Ainda estamos coletando esses dados. Aguarde só mais um pouco.')
         
